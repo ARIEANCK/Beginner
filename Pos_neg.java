0ad568b4339cf7bd6248510d83c7db2b454ea1c1
ARIEANCK <40585085+ARIEANCK@users.noreply.github.com>
2018-06-26 14:13:13 +0530

    Update Pos_neg.java

--- Pos_neg.java
+++ Pos_neg.java
@@ -3,16 +3,19 @@ public class Pos_neg {
 	public static void main(String[] args) {
 	    Scanner in=new Scanner(System.in);
 	    int N=0;
-		try {
+		try 
+		{
 			N = in.nextInt();
-		} catch (Exception e) {
+			if(N>0)
+			System.out.println("Postive");
+			else if(N<0)
+			System.out.println("Negative");
+			else
+			System.out.println("Zero");
+		} 
+		catch (Exception e) 
+		{
 			System.out.println("Invalid Input");
 		}
-	    if(N>0)
-		System.out.println("Postive");
-		else if(N<0)
-		System.out.println("Negative");
-		else
-		System.out.println("Zero");
 	}
 }
