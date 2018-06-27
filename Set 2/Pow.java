c1290efbce47c7e9694848348730500a9d9099a8
ARIEANCK <40585085+ARIEANCK@users.noreply.github.com>
2018-06-27 20:34:09 +0530

    Update Pow.java

--- Set 2/Pow.java
+++ Set 2/Pow.java
@@ -1,9 +1,5 @@
-package set2;
-
 import java.util.Scanner;
-
 public class Pow {
-
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int n=in.nextInt(),p=in.nextInt(),r=1;
@@ -11,5 +7,4 @@ public class Pow {
 			r*=n;
 		System.out.println(r);
 	}
-
 }
