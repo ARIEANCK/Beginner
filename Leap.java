e2e024278f409fc3579c39634706febd58e9fa13
ARIEANCK <40585085+ARIEANCK@users.noreply.github.com>
2018-06-27 10:27:46 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Leap {
+	public static void main(String[] args) {
+	    Scanner in=new Scanner(System.in);
+	    int N=0;
+		try 
+		{
+			N = in.nextInt();
+			if(N%4==0 && N%400!=0)
+		        System.out.println("Leap Year");
+		    else
+		        System.out.println("Not Leap Year");
+		} 
+		catch (Exception e) 
+		{
+			System.out.println("Invalid Input");
+		}
+	}
+}
