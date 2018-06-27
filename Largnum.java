a4cf8912ae3d0e1e24a66bca45006a8b0f47d371
ARIEANCK <40585085+ARIEANCK@users.noreply.github.com>
2018-06-27 10:02:19 +0530

    Update Largnum.java

--- Largnum.java
+++ Largnum.java
@@ -1,1 +1,21 @@
-
+import java.util.Scanner;
+public class Largnum {
+	public static void main(String[] args) {
+	    Scanner in=new Scanner(System.in);
+	    int N=0,max=-999;
+		try 
+		{
+		    for(int i=0;i<3;i++)
+		    {
+			N = in.nextInt();
+			if(max<N)
+			    max=N;
+		    }
+		    System.out.println(max);
+		} 
+		catch (Exception e) 
+		{
+			System.out.println("Invalid Input");
+		}
+	}
+}
